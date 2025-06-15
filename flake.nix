edc762832b3e2e44ee5d9143a1d6eac9ddee7203
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-06-14 21:45:00 -0400

    feat: add colmena flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,17 @@
 {
   description = "Homelab NixOS Flake";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://colmena.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     disko = {
@@ -12,82 +23,84 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    colmena.url = "github:zhaofengli/colmena";
   };
 
-  outputs =
-    { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in
-    {
-      checks = {
-        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            check-added-large-files.enable = true;
-            check-yaml.enable = true;
-            # deadnix.enable = true;
-            detect-private-keys.enable = true;
-            end-of-file-fixer.enable = true;
-            nixfmt-rfc-style.enable = true;
-            trim-trailing-whitespace.enable = true;
-          };
-        };
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        inherit (self.checks.pre-commit-check) shellHook;
-
-        buildInputs = [ self.checks.pre-commit-check.enabledPackages ];
-
-        packages = with pkgs; [
-          age
-          colmena
-          just
-          nil
-          nixfmt-rfc-style
-          sops
-        ];
-      };
-
-      colmena = {
-        meta = {
-          nixpkgs = import nixpkgs { inherit system; };
-
-          nodeSpecialArgs = {
-            unraid-nixos-01 = {
-              meta = {
-                hostname = "unraid-nixos-01";
-                user = "thamenato";
-              };
-            };
-          };
-        };
-
-        defaults = {
-          imports = [
-            inputs.disko.nixosModules.disko
-            inputs.sops-nix.nixosModules.sops
-          ];
-          deployment.buildOnTarget = true;
-        };
-
-        unraid-nixos-01 = {
-          imports = [ ./nixos/vms/unraid-nixos-01 ];
-
-          deployment = {
-            targetHost = "10.0.10.3";
-            targetUser = "thamenato";
-            tags = [
-              "unraid"
-              "vm"
-            ];
-          };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    checks = {
+      pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          check-added-large-files.enable = true;
+          check-yaml.enable = true;
+          # deadnix.enable = true;
+          detect-private-keys.enable = true;
+          end-of-file-fixer.enable = true;
+          alejandra.enable = true;
+          trim-trailing-whitespace.enable = true;
         };
       };
     };
+
+    devShells.${system}.default = pkgs.mkShell {
+      inherit (self.checks.pre-commit-check) shellHook;
+
+      buildInputs = [self.checks.pre-commit-check.enabledPackages];
+
+      packages = with pkgs; [
+        age
+        just
+        nil
+        alejandra
+        sops
+        inputs.colmena.packages.${system}.colmena
+      ];
+    };
+
+    colmenaHive = inputs.colmena.lib.makeHive {
+      meta = {
+        nixpkgs = import nixpkgs {inherit system;};
+
+        nodeSpecialArgs = {
+          unraid-nixos-01 = {
+            meta = {
+              hostname = "unraid-nixos-01";
+              user = "thamenato";
+            };
+          };
+        };
+      };
+
+      defaults = {
+        imports = [
+          inputs.disko.nixosModules.disko
+          inputs.sops-nix.nixosModules.sops
+        ];
+        deployment.buildOnTarget = true;
+      };
+
+      unraid-nixos-01 = {
+        imports = [./nixos/vms/unraid-nixos-01];
+
+        deployment = {
+          targetHost = "10.0.10.3";
+          targetUser = "thamenato";
+          tags = [
+            "unraid"
+            "vm"
+          ];
+        };
+      };
+    };
+  };
 }
